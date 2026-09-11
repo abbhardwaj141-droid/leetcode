0db1eb75844941acb34cc39cf7a6becfe3f2dc27
abbhardwaj141-droid <abbhardwaj141@gmail.com>
2026-09-12 01:19:46 +0530

    Time: 5 ms (99.41%) | Memory: 47.4 MB (96.29%) - LeetSync

--- 4087-maximum-substrings-with-distinct-start/maximum-substrings-with-distinct-start.java
+++ 4087-maximum-substrings-with-distinct-start/maximum-substrings-with-distinct-start.java
@@ -1,9 +1,21 @@
 class Solution {
     public int maxDistinct(String s) {
-        HashSet<Character> set = new HashSet<>();
+        // HashSet<Character> set = new HashSet<>();
+        // for (int i = 0; i < s.length(); i++) {
+        //     set.add(s.charAt(i));
+        // }
+        // return set.size();
+
+        int[] fre = new int[26];
         for (int i = 0; i < s.length(); i++) {
-            set.add(s.charAt(i));
+            fre[s.charAt(i) - 'a']++;
         }
-        return set.size();
+        int count = 0;
+        for (int i = 0; i < fre.length; i++) {
+            if (fre[i] != 0) {
+                count++;
+            }
+        }
+        return count;
     }
 }
